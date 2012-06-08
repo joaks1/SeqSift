f2328ea84b27c23412ae217cf0ed6e229ccd14ba
Jamie Oaks <joaks1@gmail.com>
2012-06-08 01:46:28 -0500

    added dataio and associated tests

---
+++ seqsift/dataio.py
@@ -0,0 +1,172 @@
+#! /usr/bin/env python
+
+import sys
+import os
+import tempfile
+import cPickle
+import contextlib
+
+from Bio.Alphabet import IUPAC
+from Bio import SeqIO, AlignIO, Entrez
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+
+from seqsift.utils import VALID_DATA_TYPES
+from seqsift.utils.messaging import get_logger
+
+_LOG = get_logger(__name__)
+
+class BufferedIter(object):
+    def __init__(self, obj_iter):
+        self._tmp = tempfile.TemporaryFile()
+        self._pickler = cPickle.Pickler(self._tmp)
+        self._unpickler = cPickle.Unpickler(self._tmp)
+        for obj in obj_iter:
+            self._pickler.dump(obj)
+
+    def iter(self):
+        self._tmp.seek(0)
+        while True:
+            try:
+                yield self._unpickler.load()
+            except EOFError:
+                break
+
+def get_tmp_handle(file_obj=None, rewind=True):
+    tmp = tempfile.TemporaryFile()
+    if not file_obj:
+        return tmp
+    elif isinstance(file_obj, str):
+        f = open(file_obj, 'rU')
+    else:
+        f = file_obj
+    for line in f:
+        tmp.write(line)
+    if rewind:
+        tmp.seek(0)
+    return tmp
+
+def get_state_alphabet(data_type, ambiguities=True):
+    if data_type.lower() == 'dna':
+        if ambiguities:
+            return IUPAC.ambiguous_dna
+        else:
+            return IUPAC.unambiguous_dna
+    elif data_type.lower() == 'rna':
+        if ambiguities:
+            return IUPAC.ambiguous_rna
+        else:
+            return IUPAC.unambiguous_rna
+    elif data_type.lower() == 'protein' or data_type.lower() == 'aa':
+        if ambiguities:
+            return IUPAC.extended_protein
+        else:
+            return IUPAC.protein
+    else:
+        raise ValueError(
+                "'{0!r}' is not a valid data type. Options:\n\t{1}".format(
+                        data_type, ", ".join(VALID_DATA_TYPES)))
+
+def read_seq(file_obj, format, data_type, ambiguities=True):
+    """
+    Returns a single SeqRecord from a file containing exactly one sequence
+    record.
+    """
+    _LOG.info("reading sequence from {0!r}.".format(file_obj))
+    return SeqIO.read(file_obj,
+            format=format,
+            alphabet=get_state_alphabet(data_type, ambiguities))
+
+def get_seq_iter(file_obj, format, data_type, ambiguities=True):
+    """
+    Returns a SeqRecord iterator from a sequence file.
+    """
+    _LOG.info("parsing SeqRecord iterator from {0!r}".format(file_obj))
+    return SeqIO.parse(file_obj,
+            format=format,
+            alphabet=get_state_alphabet(data_type, ambiguities))
+
+def get_buffered_seq_iter(file_obj, format, data_type, ambiguities=True):
+    return BufferedIter(get_seq_iter(file_obj,
+            format=format,
+            data_type=data_type,
+            ambiguities=ambiguities))
+
+def get_indexed_seq_iter(file_path, format, data_type, key_function=None,
+        ambiguities=True):
+    """
+    Returns an indexed SeqRecord iterator from a sequence file.
+    Only supports sequential file formats (e.g., fasta and genbank).
+    The iterator acts like a dict, but only parses a sequence from
+    a file when needed. For large sequence files, this is memory-efficient
+    alternative to reading all the sequences into a dict.
+    """
+    _LOG.info("parsing indexed SeqRecord iterator from {0!r}.".format(
+            file_path))
+    return SeqIO.index(file_path,
+            format=format,
+            alphabet=get_state_alphabet(data_type, ambiguities),
+            key_function=key_function)
+
+def get_seq_dict(file_obj, format, data_type, ambiguities=True):
+    """
+    Returns a dict of SeqRecords from a sequenc file.
+    This loads all the sequences in the file into memory. This is
+    efficient for small sequence files, but may cause memory issues
+    with large files.
+    """
+    return SeqIO.to_dict(get_seq_iter(file_obj,
+            format=format,
+            data_type=data_type,
+            ambiguities=ambiguities))
+
+def convert_format(in_file, in_format, out_file, out_format, data_type,
+        ambiguities=True):
+    _LOG.info("converting {in_format}-formatted file {in_file!r} to "
+              "{out_format}-formatted file {out_file!r}.".format(
+                    in_file=in_file,
+                    in_format=in_format,
+                    out_file=out_file,
+                    out_format=out_format))
+    nseqs = SeqIO.convert(
+            in_file=in_file,
+            in_format=in_format,
+            out_file=out_file,
+            out_format=out_format,
+            alphabet=get_state_alphabet(data_type, ambiguities))
+    return nseqs
+
+def get_entrez_database(data_type):
+    dt = data_type.lower()
+    if dt == 'dna' or dt == 'rna':
+        return 'nuccore'
+    elif dt == 'protein' or dt == 'aa':
+        return 'protein'
+    else:
+        raise ValueError(
+                "'{0!r}' is not a valid data type. Options:\n\t{1}".format(
+                        data_type, ", ".join(VALID_DATA_TYPES)))
+
+def get_gb_handle(gi_list, db, rettype, retmode='text', tmp_file=False):
+    _LOG.info("fetching genbank ids {0}".format(gi_list))
+    if isinstance(gi_list, str):
+        ids = gi_list
+    else:
+        ids = ",".join(gi_list)
+    if len(gi_list) > 10:
+        post = Entrez.read(Entrez.epost(db=db, id=ids))
+        webenv = post["WebEnv"]
+        query_key = post["QueryKey"]
+        h = Entrez.efetch(db=db, webenv=webenv, query_key=query_key,
+                rettype=rettype, retmode=retmode)
+    else:
+        h = Entrez.efetch(db=db, id=ids, rettype=rettype, retmode=retmode)
+    if tmp_file:
+        return get_tmp_handle(h, rewind=True)
+    else:
+        return h
+
+def fetch_gb_seqs(gi_list, data_type, parse_function=get_seq_iter):
+    db = get_entrez_database(data_type)
+    file_obj = get_gb_handle(gi_list, db=db, rettype='gb', retmode='text')
+    return parse_function(file_obj, format='gb', data_type=data_type)
---
+++ seqsift/test/test_dataio.py
@@ -0,0 +1,384 @@
+#! /usr/bin/env python
+
+import os
+import sys
+import unittest
+import itertools
+
+from Bio.Alphabet import IUPAC
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+from Bio import SeqIO
+
+from seqsift.dataio import *
+from seqsift.test.support import package_paths
+from seqsift.test.support.extended_test_case import SeqSiftTestCase
+from seqsift.utils import mkdr
+from seqsift.utils.messaging import get_logger
+
+_LOG = get_logger(__name__)
+
+class GetStateAlphabetTestCase(unittest.TestCase):
+    def test_error_handling(self):
+        self.assertRaises(ValueError, get_state_alphabet, 'bogus')
+    def test_ambiguous_dna(self):
+        alphabet = get_state_alphabet('dna', ambiguities=True)
+        self.assertIsInstance(alphabet, IUPAC.IUPACAmbiguousDNA)
+    def test_unambiguous_dna(self):
+        alphabet = get_state_alphabet('dna', ambiguities=False)
+        self.assertIsInstance(alphabet, IUPAC.IUPACUnambiguousDNA)
+    def test_ambiguous_rna(self):
+        alphabet = get_state_alphabet('rna', ambiguities=True)
+        self.assertIsInstance(alphabet, IUPAC.IUPACAmbiguousRNA)
+    def test_unambiguous_rna(self):
+        alphabet = get_state_alphabet('rna', ambiguities=False)
+        self.assertIsInstance(alphabet, IUPAC.IUPACUnambiguousRNA)
+    def test_ambiguous_protein(self):
+        for arg in ['protein', 'aa']:
+            alphabet = get_state_alphabet(arg, ambiguities=True)
+            self.assertIsInstance(alphabet, IUPAC.ExtendedIUPACProtein)
+    def test_unambiguous_protein(self):
+        for arg in ['protein', 'aa']:
+            alphabet = get_state_alphabet(arg, ambiguities=False)
+            self.assertIsInstance(alphabet, IUPAC.IUPACProtein)
+
+class BufferedIterTestCase(SeqSiftTestCase):
+    def test_simple(self):
+        objs = [SeqRecord(Seq('AAAA'), id='a'),
+                SeqRecord(Seq('CCCC'), id='c'),
+                SeqRecord(Seq('GGGG'), id='g'),
+                SeqRecord(Seq('TTTT'), id='t')]
+        buffered_iter = BufferedIter(objs)
+        for i in range(3):
+            self.assertSameIDs(objs, buffered_iter.iter())
+            self.assertSameSequences(objs, buffered_iter.iter())
+
+class GetTmpHandleTestCase(unittest.TestCase):
+    def setUp(self):
+        self.path = package_paths.data_path('primates.partitions.txt')
+        self.line = 'another line\n'
+
+    def test_empty_handle(self):
+        tmp = get_tmp_handle()
+        self.assertTrue(hasattr(tmp, 'close'))
+        self.assertEqual(tmp.read(), '')
+        tmp.close()
+
+    def test_handle_bottom(self):
+        tmp = get_tmp_handle(self.path, rewind=False)
+        self.assertTrue(hasattr(tmp, 'close'))
+        self.assertEqual(tmp.read(), '')
+        tmp.write(self.line)
+        tmp.seek(0)
+        self.assertEqual(
+                tmp.read(),
+                open(self.path, 'rU').read() + self.line)
+        tmp.close()
+
+    def test_handle_top(self):
+        tmp = get_tmp_handle(self.path, rewind=True)
+        self.assertTrue(hasattr(tmp, 'close'))
+        self.assertEqual(tmp.read(), open(self.path, 'rU').read())
+
+class ReadSeqTestCase(SeqSiftTestCase):
+    def setUp(self):
+        self.mkTestDir()
+        self.gb_path = package_paths.data_path('JF314862.gb')
+        self.fasta_path = package_paths.data_path('JF314862.fasta')
+        self.seq_str = 'CATCATCAACATCATCGTGCCCTGCGTGCTCATCTCCTTCGTGGCTGTGC' + \
+                       'TCGTCTACTTTCTGCCTGCCAAGGGTAACGCTGGCACCAGGCGGCTGTGG' + \
+                       'GACTGCCTGTGCCATAGGCGTGAAGAGGGCAGGCCATGTGGCTGGGCAGA' + \
+                       'GGGAGGGAAGTGGGGGACAGCCACCGCTGGGAGACTGGCACCTGGGCCCA' + \
+                       'GTGCCCGTCATTTCCCCATCACATGGGCTTGGGGACATGGAAGCCAGTCC' + \
+                       'TGTGGGAGCAGACAGACACTCCCGGCTGCCGTGTCAGTCCTTAGGGCTGG' + \
+                       'CTGGACTCTCTCTGCACAGCCTCCCACTGTCAGTCCCAGGACCATCCATG' + \
+                       'TCCTAGGCATGTCTAGGCAGAGCCAGGCCCTTTCCAGGTGCCCTGGGACC' + \
+                       'CCGTCTCACGTGTCGATCCCCTCACTCTCCACATCCTGGCAGCGGGTGGG' + \
+                       'CAGAAGTGCACCGTCTCCATCAATGTCC'
+        self.fasta_str = ">s1\nACGTGCTATCTATCGTATTTAG\n"
+        self.small_fasta = self.getTestFile('small.fasta')
+        out = open(self.small_fasta, 'w')
+        out.write(self.fasta_str)
+        out.close()
+
+    def test_small_fasta(self):
+        seq = read_seq(self.small_fasta, format='fasta', data_type='dna')
+        self.assertIsInstance(seq, SeqRecord)
+        self.assertEqual(seq.id, 's1')
+        self.assertEqual(str(seq.seq), 'ACGTGCTATCTATCGTATTTAG')
+
+    def test_fasta(self):
+        seq = read_seq(self.fasta_path, format='fasta', data_type='dna')
+        self.assertIsInstance(seq, SeqRecord)
+        self.assertEqual(str(seq.seq), self.seq_str)
+
+    def test_genbank(self):
+        seq = read_seq(self.gb_path, format='gb', data_type='dna')
+        self.assertIsInstance(seq, SeqRecord)
+        self.assertEqual(str(seq.seq), self.seq_str)
+        self.assertEqual(seq.name, 'JF314862')
+        self.assertEqual(seq.id, 'JF314862.1')
+
+class GetSeqIterTestCase(SeqSiftTestCase):
+    def setUp(self):
+        self.gb_path = package_paths.data_path('JF314863-JF314866.gb')
+        self.fasta_path = package_paths.data_path('JF314863-JF314866.fasta')
+        self.names = ['JF' + str(x) for x in range(314863, 314867)]
+        self.ids = [str(x) + '.1' for x in self.names]
+
+    def test_genbank(self):
+        seq_iter = get_seq_iter(self.gb_path, format='gb', data_type='dna')
+        names = []
+        ids = []
+        for seq in seq_iter:
+            self.assertIsInstance(seq, SeqRecord)
+            names.append(seq.name)
+            ids.append(seq.id)
+        self.assertEqual(len(names), 4)
+        self.assertEqual(sorted(names), sorted(self.names))
+        self.assertEqual(sorted(ids),
+                sorted(self.ids))
+
+    def test_fasta(self):
+        seq_iter = get_seq_iter(self.fasta_path, format='fasta',
+                data_type='dna')
+        names = []
+        for seq in seq_iter:
+            self.assertIsInstance(seq, SeqRecord)
+            n = seq.name.split('|')[3]
+            names.append(n)
+        self.assertEqual(len(names), 4)
+        self.assertEqual(sorted(names), sorted(self.ids))
+
+    def test_genbank_fasta(self):
+        si1 = get_seq_iter(self.gb_path, format='gb', data_type='dna')
+        si2 = get_seq_iter(self.fasta_path, format='fasta', data_type='dna')
+        self.assertSameSequenceData(si1, si2)
+
+class GetBufferedSeqIterTestCase(SeqSiftTestCase):
+    def setUp(self):
+        self.gb_path = package_paths.data_path('JF314863-JF314866.gb')
+        self.seqs = get_buffered_seq_iter(self.gb_path, format='gb',
+                data_type='dna')
+
+    def test_buffer(self):
+        s1 = [s for s in self.seqs.iter()]
+        s2 = [s for s in self.seqs.iter()]
+        self.assertSameData(s1, s2)
+
+class GetIndexedSeqIterTestCase(SeqSiftTestCase):
+    def setUp(self):
+        self.gb_path = package_paths.data_path('JF314863-JF314866.gb')
+        self.seqs = get_indexed_seq_iter(self.gb_path, format='gb',
+                data_type='dna')
+
+    def test_indexing(self):
+        ids = []
+        for id_str in self.seqs:
+            self.assertIsInstance(self.seqs[id_str], SeqRecord)
+            self.assertEqual(self.seqs[id_str].id, id_str)
+            ids.append(id_str)
+        for i in range(2):
+            for id_str in ids:
+                seq = self.seqs[id_str]
+                self.assertIsInstance(seq, SeqRecord)
+                self.assertEqual(seq.id, id_str)
+
+class GetSeqDictTestCase(SeqSiftTestCase):
+    def setUp(self):
+        self.gb_path = package_paths.data_path('JF314863-JF314866.gb')
+        self.seqs = get_seq_dict(self.gb_path, format='gb', data_type='dna')
+
+    def test_dict(self):
+        self.assertIsInstance(self.seqs, dict)
+        for k, v in self.seqs.iteritems():
+            self.assertIsInstance(v, SeqRecord)
+            self.assertEqual(v.id, k)
+
+class ConvertFormatTestCase(SeqSiftTestCase):
+    def setUp(self):
+        self.mkTestDir()
+
+    def test_primates(self):
+        formats = {'fasta': '.fasta', 'phylip-relaxed': '.phylip',
+                'nexus': '.nexus'}
+        for in_format, in_ext in formats.iteritems():
+            in_file = package_paths.data_path('primates' + in_ext)
+            for out_format, out_ext in formats.iteritems():
+                out_file = self.getTestFile('primates' + out_ext)
+                n = convert_format(in_file=in_file,
+                        in_format=in_format,
+                        out_file=out_file,
+                        out_format=out_format,
+                        data_type='dna')
+                self.assertEqual(n, 12)
+                in_seqs = SeqIO.parse(in_file, format=in_format,
+                        alphabet=IUPAC.ambiguous_dna)
+                out_seqs = SeqIO.parse(out_file, format=out_format,
+                        alphabet=IUPAC.ambiguous_dna)
+                self.assertSameData(in_seqs, out_seqs)
+
+    def test_limnonectes(self):
+        formats = {'fasta': '.fasta', 'phylip-relaxed': '.phylip',
+                'nexus': '.nexus'}
+        for in_format, in_ext in formats.iteritems():
+            in_file = package_paths.data_path('limnonectes' + in_ext)
+            for out_format, out_ext in formats.iteritems():
+                out_file = self.getTestFile('limnonectes' + out_ext)
+                n = convert_format(in_file=in_file,
+                        in_format=in_format,
+                        out_file=out_file,
+                        out_format=out_format,
+                        data_type='dna')
+                self.assertEqual(n, 80)
+                in_seqs = SeqIO.parse(in_file, format=in_format,
+                        alphabet=IUPAC.ambiguous_dna)
+                out_seqs = SeqIO.parse(out_file, format=out_format,
+                        alphabet=IUPAC.ambiguous_dna)
+                self.assertSameData(in_seqs, out_seqs)
+
+    def test_caenophidia(self):
+        formats = {'fasta': '.fasta', 'phylip-relaxed': '.phylip',
+                'nexus': '.nexus'}
+        for in_format, in_ext in formats.iteritems():
+            in_file = package_paths.data_path('caenophidia' + in_ext)
+            for out_format, out_ext in formats.iteritems():
+                out_file = self.getTestFile('caenophidia' + out_ext)
+                n = convert_format(in_file=in_file,
+                        in_format=in_format,
+                        out_file=out_file,
+                        out_format=out_format,
+                        data_type='protein')
+                self.assertEqual(n, 114)
+                in_seqs = SeqIO.parse(in_file, format=in_format,
+                        alphabet=IUPAC.extended_protein)
+                out_seqs = SeqIO.parse(out_file, format=out_format,
+                        alphabet=IUPAC.extended_protein)
+                self.assertSameData(in_seqs, out_seqs)
+
+class GetEntrezDatabaseTestCase(unittest.TestCase):
+    def test_error_handling(self):
+        self.assertRaises(ValueError, get_entrez_database, 'bogus')
+
+    def test_nuccore(self):
+        db = get_entrez_database('dna')
+        self.assertEqual(db, 'nuccore')
+        db = get_entrez_database('rna')
+        self.assertEqual(db, 'nuccore')
+
+    def test_protein(self):
+        db = get_entrez_database('aa')
+        self.assertEqual(db, 'protein')
+        db = get_entrez_database('protein')
+        self.assertEqual(db, 'protein')
+
+class GetGbHandleTestCase(SeqSiftTestCase):
+    def setUp(self):
+        self.id = '354698774'
+        self.singleton_fasta = package_paths.data_path('JF314862.fasta')
+        self.singleton_gb = package_paths.data_path('JF314862.gb')
+        self.id_list = ['354698776', '354698778', '354698780', '354698782']
+        self.ids = ','.join(self.id_list)
+        self.multi_fasta = package_paths.data_path('JF314863-JF314866.fasta')
+        self.multi_gb = package_paths.data_path('JF314863-JF314866.gb')
+
+    def test_singleton_fasta(self):
+        h = get_gb_handle(self.id, db='nuccore', rettype='fasta',
+            retmode='text', tmp_file=False)
+        seqs1 = SeqIO.parse(h, format='fasta', alphabet=IUPAC.ambiguous_dna)
+        seqs2 = SeqIO.parse(self.singleton_fasta, format='fasta',
+                alphabet=IUPAC.ambiguous_dna)
+        self.assertSameData(seqs1, seqs2)
+        h.close()
+
+    def test_singleton_gb(self):
+        h = get_gb_handle(self.id, db='nuccore', rettype='gb',
+            retmode='text', tmp_file=False)
+        seqs1 = SeqIO.parse(h, format='gb', alphabet=IUPAC.ambiguous_dna)
+        seqs2 = SeqIO.parse(self.singleton_gb, format='gb',
+                alphabet=IUPAC.ambiguous_dna)
+        self.assertSameData(seqs1, seqs2)
+        h.close()
+
+    def test_multi_fasta(self):
+        h = get_gb_handle(self.ids, db='nuccore', rettype='fasta',
+            retmode='text', tmp_file=False)
+        seqs1 = SeqIO.parse(h, format='fasta', alphabet=IUPAC.ambiguous_dna)
+        seqs2 = SeqIO.parse(self.multi_fasta, format='fasta',
+                alphabet=IUPAC.ambiguous_dna)
+        self.assertSameData(seqs1, seqs2)
+        h.close()
+
+    def test_multi_gb(self):
+        h = get_gb_handle(self.ids, db='nuccore', rettype='gb',
+            retmode='text', tmp_file=False)
+        seqs1 = SeqIO.parse(h, format='gb', alphabet=IUPAC.ambiguous_dna)
+        seqs2 = SeqIO.parse(self.multi_gb, format='gb',
+                alphabet=IUPAC.ambiguous_dna)
+        self.assertSameData(seqs1, seqs2)
+        h.close()
+
+    def test_singleton_fasta_tmp_file(self):
+        h = get_gb_handle(self.id, db='nuccore', rettype='fasta',
+            retmode='text', tmp_file=True)
+        seqs1 = SeqIO.parse(h, format='fasta', alphabet=IUPAC.ambiguous_dna)
+        seqs2 = SeqIO.parse(self.singleton_fasta, format='fasta',
+                alphabet=IUPAC.ambiguous_dna)
+        self.assertSameData(seqs1, seqs2)
+        h.close()
+
+    def test_singleton_gb_tmp_file(self):
+        h = get_gb_handle(self.id, db='nuccore', rettype='gb',
+            retmode='text', tmp_file=True)
+        seqs1 = SeqIO.parse(h, format='gb', alphabet=IUPAC.ambiguous_dna)
+        seqs2 = SeqIO.parse(self.singleton_gb, format='gb',
+                alphabet=IUPAC.ambiguous_dna)
+        self.assertSameData(seqs1, seqs2)
+        h.close()
+
+    def test_multi_fasta_tmp_file(self):
+        h = get_gb_handle(self.ids, db='nuccore', rettype='fasta',
+            retmode='text', tmp_file=True)
+        seqs1 = SeqIO.parse(h, format='fasta', alphabet=IUPAC.ambiguous_dna)
+        seqs2 = SeqIO.parse(self.multi_fasta, format='fasta',
+                alphabet=IUPAC.ambiguous_dna)
+        self.assertSameData(seqs1, seqs2)
+        h.close()
+
+    def test_multi_gb_tmp_file(self):
+        h = get_gb_handle(self.ids, db='nuccore', rettype='gb',
+            retmode='text', tmp_file=True)
+        seqs1 = SeqIO.parse(h, format='gb', alphabet=IUPAC.ambiguous_dna)
+        seqs2 = SeqIO.parse(self.multi_gb, format='gb',
+                alphabet=IUPAC.ambiguous_dna)
+        self.assertSameData(seqs1, seqs2)
+        h.close()
+
+    def test_multi_fasta_id_list(self):
+        h = get_gb_handle(self.id_list, db='nuccore', rettype='fasta',
+            retmode='text', tmp_file=False)
+        seqs1 = SeqIO.parse(h, format='fasta', alphabet=IUPAC.ambiguous_dna)
+        seqs2 = SeqIO.parse(self.multi_fasta, format='fasta',
+                alphabet=IUPAC.ambiguous_dna)
+        self.assertSameData(seqs1, seqs2)
+        h.close()
+
+    def test_multi_gb_id_list(self):
+        h = get_gb_handle(self.id_list, db='nuccore', rettype='gb',
+            retmode='text', tmp_file=False)
+        seqs1 = SeqIO.parse(h, format='gb', alphabet=IUPAC.ambiguous_dna)
+        seqs2 = SeqIO.parse(self.multi_gb, format='gb',
+                alphabet=IUPAC.ambiguous_dna)
+        self.assertSameData(seqs1, seqs2)
+        h.close()
+
+class FetchGbSeqsTestCase(SeqSiftTestCase):
+    def setUp(self):
+        self.id_list = ['354698776', '354698778', '354698780', '354698782']
+        self.multi_gb = package_paths.data_path('JF314863-JF314866.gb')
+
+    def test_basic(self):
+        seqs1 = fetch_gb_seqs(self.id_list, data_type='dna')
+        seqs2 = SeqIO.parse(self.multi_gb, format='gb',
+                alphabet=IUPAC.ambiguous_dna)
+        self.assertSameData(seqs1, seqs2)
