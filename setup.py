66db188a7c71b0222273bb9a32312744818a50a5
Jamie Oaks <joaks1@gmail.com>
2012-06-21 17:24:15 -0500

    added scripts to setup()

--- setup.py
+++ setup.py
@@ -2,6 +2,10 @@
 
 from setuptools import setup, find_packages
 import sys, os
+import glob
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+SCRIPTS = glob.glob(os.path.join(BASE_DIR, 'scripts', '*.py'))
 
 version = '0.1'
 
@@ -17,11 +21,12 @@ A python package for vetting DNA sequence data""",
       url='',
       license='GPL',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      scripts=SCRIPTS,
       include_package_data=True,
       zip_safe=True,
       test_suite="seqsift.test",
       install_requires=[
-          # -*- Extra requirements: -*-
+          'biopython'
       ],
       entry_points="""
       # -*- Entry points: -*-
