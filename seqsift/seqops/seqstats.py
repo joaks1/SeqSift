11bbf5cb7566c8703d23152eac576abffb66365b
Jamie Oaks <joaks1@gmail.com>
2012-09-12 17:55:08 -0500

    Beginning work on seqstats.py

---
+++ seqsift/seqops/seqstats.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+
+import sys
+import os
+
+from seqsift.dataio import BufferedIter
+from seqsift.utils.errors import AlignmentError
+from seqsift.utils.messaging import get_logger
+
+_LOG = get_logger(__name__)
+
+def column_frequencies(seq_iter, character_list=['-','?']):
+    seqs = BufferedIter(seq_iter)
+    column_counts = []
+    align_length = None
+    for i, seq_record in enumerate(seqs.iter()):
+        if align_length == None:
+            align_length = len(seq_record)
+            gaps = [0] * align_length
+        else:
+            if len(seq_record) != align_length:
+                raise AlignmentError('Sequence {0} has unexpected '
+                        'length {1}.'.format(seq_record.name, len(seq_record)))
+        for j, character in enumerate(seq_record.seq):
+            if character in character_list:
+                column_counts[j] += 1
+    column_freqs = [count / float(i + 1) for count in column_counts]
+    return column_freqs, seqs
+    
+
