9da73637b52b0834d980d7ae9c9927c618399d00
Jamie Oaks <joaks1@gmail.com>
2012-09-18 02:47:19 -0500

    Updating dataio functions to guess file format.
    All of the sequence parsing functions in seqsift/utils/dataio.py
    now use the FileFormats global instance FILE_FORMATS to guess
    the file format from the file argument if the format is not
    provided.

--- seqsift/utils/dataio.py
+++ seqsift/utils/dataio.py
@@ -10,10 +10,12 @@ from Bio import SeqIO, AlignIO
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 
-from seqsift.utils import VALID_DATA_TYPES
+from seqsift.utils import VALID_DATA_TYPES, FILE_FORMATS
 from seqsift.utils.messaging import get_logger
+from seqsift.utils.errors import FileExtensionError
 
 _LOG = get_logger(__name__)
+
 
 class BufferedIter(object):
     def __init__(self, obj_iter):
@@ -66,40 +68,50 @@ def get_state_alphabet(data_type, ambiguities=True):
                 "'{0!r}' is not a valid data type. Options:\n\t{1}".format(
                         data_type, ", ".join(VALID_DATA_TYPES)))
 
-def read_seq(file_obj, format, data_type, ambiguities=True):
+def read_seq(file_obj, format=None, data_type='dna', ambiguities=True):
     """
     Returns a single SeqRecord from a file containing exactly one sequence
     record.
     """
+    if format == None:
+        format = FILE_FORMATS.get_format_from_file_object(file_obj)
     _LOG.info("reading sequence from {0!r}.".format(file_obj))
     return SeqIO.read(file_obj,
             format=format,
             alphabet=get_state_alphabet(data_type, ambiguities))
 
-def get_seq_iter(file_obj, format, data_type, ambiguities=True):
+def get_seq_iter(file_obj, format=None, data_type='dna', ambiguities=True):
     """
     Returns a SeqRecord iterator from a sequence file.
     """
+    if format == None:
+        format = FILE_FORMATS.get_format_from_file_object(file_obj)
     _LOG.info("parsing SeqRecord iterator from {0!r}".format(file_obj))
     return SeqIO.parse(file_obj,
             format=format,
             alphabet=get_state_alphabet(data_type, ambiguities))
 
-def get_buffered_seq_iter(file_obj, format, data_type, ambiguities=True):
+def get_buffered_seq_iter(file_obj, format=None, data_type='dna',
+        ambiguities=True):
+    if format == None:
+        format = FILE_FORMATS.get_format_from_file_object(file_obj)
     return BufferedIter(get_seq_iter(file_obj,
             format=format,
             data_type=data_type,
             ambiguities=ambiguities))
 
-def get_indexed_seq_iter(file_path, format, data_type, key_function=None,
+def get_indexed_seq_iter(file_path, format=None, data_type='dna',
+        key_function=None,
         ambiguities=True):
     """
-    Returns an indexed SeqRecord iterator from a sequence file.
-    Only supports sequential file formats (e.g., fasta and genbank).
-    The iterator acts like a dict, but only parses a sequence from
-    a file when needed. For large sequence files, this is memory-efficient
-    alternative to reading all the sequences into a dict.
+    Returns an indexed SeqRecord iterator from a sequence file.  Only supports
+    sequential file formats (e.g., fasta and genbank).  The iterator acts like
+    a dict, but only parses a sequence from a file when needed. For large
+    sequence files, this is memory-efficient alternative to reading all the
+    sequences into a dict.
     """
+    if format == None:
+        format = FILE_FORMATS.get_format_from_file_object(file_path)
     _LOG.info("parsing indexed SeqRecord iterator from {0!r}.".format(
             file_path))
     return SeqIO.index(file_path,
@@ -107,20 +119,28 @@ def get_indexed_seq_iter(file_path, format, data_type, key_function=None,
             alphabet=get_state_alphabet(data_type, ambiguities),
             key_function=key_function)
 
-def get_seq_dict(file_obj, format, data_type, ambiguities=True):
+def get_seq_dict(file_obj, format=None, data_type='dna', ambiguities=True):
     """
-    Returns a dict of SeqRecords from a sequenc file.
-    This loads all the sequences in the file into memory. This is
-    efficient for small sequence files, but may cause memory issues
-    with large files.
+    Returns a dict of SeqRecords from a sequenc file.  This loads all the
+    sequences in the file into memory. This is efficient for small sequence
+    files, but may cause memory issues with large files.
     """
+    if format == None:
+        format = FILE_FORMATS.get_format_from_file_object(file_obj)
     return SeqIO.to_dict(get_seq_iter(file_obj,
             format=format,
             data_type=data_type,
             ambiguities=ambiguities))
 
-def convert_format(in_file, in_format, out_file, out_format, data_type,
+def convert_format(in_file, out_file,
+        in_format=None,
+        out_format=None,
+        data_type='dna',
         ambiguities=True):
+    if in_format == None:
+        in_format = FILE_FORMATS.get_format_from_file_object(in_file)
+    if out_format == None:
+        out_format = FILE_FORMATS.get_format_from_file_object(out_file)
     _LOG.info("converting {in_format}-formatted file {in_file!r} to "
               "{out_format}-formatted file {out_file!r}.".format(
                     in_file=in_file,
