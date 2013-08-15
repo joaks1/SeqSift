cc9f62ea80f046c9ad7d1795302eb1587a17c444
Jamie Oaks <joaks1@gmail.com>
2013-08-14 22:39:42 -0500

    More work on `fileio` module.
    Still untested and not used anywhere in the package outside the module.
    
    Working on making `OpenFile` a more comprehensive "wrapper" class around
    the `file` and `gzip.GzipFile` classes.
    
    Also, adding `process_file_arg` function, which determines whether the
    file argument is a string or not (then it assumes it is already an files
    stream), and returns a tuple of the file stream and a boolean indicating
    whether or not the function opened the file.  This can be useful for
    code that can handle a file path string or a file stream argument. The
    returned boolean can be then be used to determine whether or not the
    file should be closed at that point in the code. I.e., if the file was
    opened there, it should probably be closed. But, you may not want to
    close a file that was opened by the caller.

--- seqsift/utils/fileio.py
+++ seqsift/utils/fileio.py
@@ -8,7 +8,6 @@ from seqsift.utils.messaging import get_logger
 
 _LOG = get_logger(__name__)
 
-
 def expand_path(path):
     return os.path.abspath(os.path.expandvars(os.path.expanduser(path)))
 
@@ -25,6 +24,11 @@ def is_gzipped(file_path):
         return True
     return False
 
+def process_file_arg(file_arg, mode='rU', compresslevel = None):
+    if isinstance(file_arg, str):
+        return OpenFile(file_arg, mode, compresslevel), True
+    return file_arg, False
+
 class OpenFile(object):
     open_files = set()
     def __init__(self, *args, **kwargs):
@@ -34,52 +38,84 @@ class OpenFile(object):
         self.buffering = None
         self.compresslevel = None
         self.gzipped = False
-        self.close = True
         if kwargs.get('compresslevel', None):
             self.compresslevel = kwargs.pop('compresslevel')
         self.name = kwargs.get('name', None)
         if not self.name:
             self.name = args[0]
-        if isinstance(self.name, str):
-            self.name = expand_path(self.name)
-            if len(args) > 1:
-                self.mode = args[1]
-            else:
-                self.mode = kwargs.get('mode', 'r')
-            if len(args) > 2:
-                self.buffering = args[2]
-            else:
-                self.buffering = kwargs.get('buffering', None)
-            if os.path.isfile(self.name):
-                self.gzipped = is_gzipped(self.name)
-
-            if self.compresslevel:
-                self.mode = self.mode.strip('U+') + 'b'
-                self.file_stream = gzip.open(filename = self.name,
-                        mode = self.mode,
-                        compresslevel = self.compresslevel)
-            elif self.gzipped:
-                self.mode = self.mode.strip('U+') + 'b'
-                self.compresslevel = 9
-                self.file_stream = gzip.open(filename = self.name,
-                        mode = self.mode,
-                        compresslevel = self.compresslevel)
-            else:
-                if self.buffering:
-                    self.file_stream = open(name = self.name, mode = self.mode,
-                            buffering = self.buffering)
-                else:
-                    self.file_stream = open(name = self.name, mode = self.mode)
-            self.__class__.open_files.add(self.name)
+        self.name = expand_path(self.name)
+        if len(args) > 1:
+            self.mode = args[1]
         else:
-            self.file_stream = self.name
-            self.close = False
+            self.mode = kwargs.get('mode', 'r')
+        if len(args) > 2:
+            self.buffering = args[2]
+        else:
+            self.buffering = kwargs.get('buffering', None)
+        if os.path.isfile(self.name):
+            self.gzipped = is_gzipped(self.name)
+
+        if self.compresslevel:
+            self.mode = self.mode.strip('U+') + 'b'
+            self.file_stream = gzip.open(filename = self.name,
+                    mode = self.mode,
+                    compresslevel = self.compresslevel)
+        elif self.gzipped:
+            self.mode = self.mode.strip('U+') + 'b'
+            self.compresslevel = 9
+            self.file_stream = gzip.open(filename = self.name,
+                    mode = self.mode,
+                    compresslevel = self.compresslevel)
+        else:
+            if self.buffering:
+                self.file_stream = open(name = self.name, mode = self.mode,
+                        buffering = self.buffering)
+            else:
+                self.file_stream = open(name = self.name, mode = self.mode)
+        self.__class__.open_files.add(self.name)
+
+    def close(self):
+        self.file_stream.close()
+        self.__class__.open_files.remove(self.name)
 
     def __enter__(self):
         return self.file_stream
 
     def __exit__(self, type, value, traceback):
-        if self.close:
-            self.file_stream.close()
-            self.__class__.open_files.remove(self.name)
+        self.close()
+
+    def __iter__(self):
+        return self.file_stream.__iter__()
+
+    def write(self, string):
+        self.file_stream.write(string)
+
+    def writelines(self, sequence_of_strings):
+        self.file_stream.writelines(sequence_of_strings)
+
+    def next(self):
+        return self.file_stream.next()
+
+    def read(self):
+        return self.file_stream.read()
+
+    def readline(self):
+        return self.file_stream.readline()
+
+    def readlines(self):
+        return self.file_stream.readlines()
+
+    def _get_closed(self):
+        return self.file_stream.closed
+
+    closed = property(_get_closed)
+
+    def seek(self, i):
+        self.file_stream.seek(i)
+
+    def flush(self):
+        self.file_stream.flush()
+
+    def fileno(self):
+        return self.file_stream.fileno()
 
