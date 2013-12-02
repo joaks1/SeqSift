745ac0fbf533f0085f0ac5a4a4a4aff22ac74238
Jamie Oaks <joaks1@gmail.com>
2013-12-01 21:25:03 -0800

    Adding seq distance functions to `seqstats` module.
    Adding `get_differences`, `distance`, and `per_site_distance` functions
    to `seqstats` module. `get_differences` compares to sequences and
    returns a dict mapping the indices of differences to a tuple of the
    different character states. `distance` and `per_site_distance` use
    `get_differences` and return the number of differences and the number of
    differences divided by the sequence length, respectively.
    
    Adding tests of all three functions to `test_seqstats.py`.

--- seqsift/seqops/seqstats.py
+++ seqsift/seqops/seqstats.py
@@ -3,8 +3,10 @@
 import sys
 import os
 
+from seqsift.align import global_align
 from seqsift.utils.dataio import BufferedIter
 from seqsift.utils.errors import AlignmentError
+from seqsift.utils.alphabets import DnaAlphabet
 from seqsift.utils.messaging import get_logger
 
 _LOG = get_logger(__name__)
@@ -28,6 +30,77 @@ def column_frequencies(seq_iter, character_list=['-','?']):
     column_freqs = [count / float(i + 1) for count in column_counts]
     return column_freqs, seqs
     
-def get_differences(seq1, seq2):
-    pass
+def distance(seq1, seq2,
+        aligned = False,
+        ignore_gaps = True,
+        alphabet = None,
+        similarity_matrix = {
+            ('A', 'A'): 10, ('A', 'G'): -1, ('A', 'C'): -3, ('A', 'T'): -4,
+            ('G', 'A'): -1, ('G', 'G'):  7, ('G', 'C'): -5, ('G', 'T'): -3,
+            ('C', 'A'): -3, ('C', 'G'): -5, ('C', 'C'):  9, ('C', 'T'):  0,
+            ('T', 'A'): -4, ('T', 'G'): -3, ('T', 'C'):  0, ('T', 'T'):  8,
+        },
+        gap_cost = -5):
+    diffs, l  = get_differences(seq1 = seq1, seq2 = seq2,
+            aligned = aligned,
+            ignore_gaps = ignore_gaps,
+            alphabet = alphabet,
+            similarity_matrix = similarity_matrix,
+            gap_cost = gap_cost)
+    return len(diffs)
+
+def per_site_distance(seq1, seq2,
+        aligned = False,
+        ignore_gaps = True,
+        alphabet = None,
+        similarity_matrix = {
+            ('A', 'A'): 10, ('A', 'G'): -1, ('A', 'C'): -3, ('A', 'T'): -4,
+            ('G', 'A'): -1, ('G', 'G'):  7, ('G', 'C'): -5, ('G', 'T'): -3,
+            ('C', 'A'): -3, ('C', 'G'): -5, ('C', 'C'):  9, ('C', 'T'):  0,
+            ('T', 'A'): -4, ('T', 'G'): -3, ('T', 'C'):  0, ('T', 'T'):  8,
+        },
+        gap_cost = -5):
+    diffs, l  = get_differences(seq1 = seq1, seq2 = seq2,
+            aligned = aligned,
+            ignore_gaps = ignore_gaps,
+            alphabet = alphabet,
+            similarity_matrix = similarity_matrix,
+            gap_cost = gap_cost)
+    return len(diffs) / float(l)
+
+def get_differences(seq1, seq2,
+        aligned = False,
+        ignore_gaps = True,
+        alphabet = None,
+        similarity_matrix = {
+            ('A', 'A'): 10, ('A', 'G'): -1, ('A', 'C'): -3, ('A', 'T'): -4,
+            ('G', 'A'): -1, ('G', 'G'):  7, ('G', 'C'): -5, ('G', 'T'): -3,
+            ('C', 'A'): -3, ('C', 'G'): -5, ('C', 'C'):  9, ('C', 'T'):  0,
+            ('T', 'A'): -4, ('T', 'G'): -3, ('T', 'C'):  0, ('T', 'T'):  8,
+        },
+        gap_cost = -5):
+    if not alphabet:
+        alphabet = DnaAlphabet()
+    if not aligned:
+        seq1, seq2 = global_align(
+                [x for x in seq1 if x != alphabet.gap],
+                [x for x in seq2 if x != alphabet.gap],
+                similarity_matrix = similarity_matrix,
+                gap_cost = gap_cost)
+    if len(seq1) != len(seq2):
+        raise AlignmentError('Sequences are not aligned')
+    residue_codes = alphabet.all_residue_codes
+    diffs = {}
+    for i in range(len(seq1)):
+        if seq1[i].upper() == seq2[i].upper():
+            continue
+        if (seq1[i] == alphabet.gap) or (seq2[i] == alphabet.gap):
+            if not ignore_gaps:
+                diffs[i] = (seq1[i].upper(), seq2[i].upper())
+            continue
+        s1 = set(residue_codes[seq1[i].upper()])
+        s2 = set(residue_codes[seq2[i].upper()])
+        if not s1.intersection(s2):
+            diffs[i] = (seq1[i].upper(), seq2[i].upper())
+    return diffs, len(seq1)
 
--- seqsift/test/test_seqstats.py
+++ seqsift/test/test_seqstats.py
@@ -10,12 +10,103 @@ from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 from Bio.Alphabet import IUPAC
 
-from seqsift.seqops.seqstats import *
+from seqsift.seqops import seqstats
 from seqsift.test.support import package_paths
 from seqsift.test.support.extended_test_case import SeqSiftTestCase
 from seqsift.utils.messaging import get_logger
 
 _LOG = get_logger(__name__)
+
+class DistanceTestCase(unittest.TestCase):
+    def test_aligned(self):
+        seq1 = 'AC--GTNAC-TYATR'
+        seq2 = 'ACN-GTAAC--CATT'
+        d = seqstats.distance(seq1, seq2, aligned = True)
+        self.assertEqual(d, 1)
+        dps = seqstats.per_site_distance(seq1, seq2, aligned = True)
+        self.assertAlmostEqual(dps, 1 / float(15))
+
+        d = seqstats.distance(seq1, seq2, aligned = True, ignore_gaps = False)
+        self.assertEqual(d, 3)
+        dps = seqstats.per_site_distance(seq1, seq2, aligned = True,
+                ignore_gaps = False)
+        self.assertAlmostEqual(dps, 3 / float(15))
+
+    def test_unaligned(self):
+        seq1 = 'AC--GTNAC-TYATR'
+        seq2 = 'ACN-GTAAC--CATT'
+        d = seqstats.distance(seq1, seq2, aligned = False,
+                ignore_gaps = False)
+        self.assertEqual(d, 3)
+        dps = seqstats.per_site_distance(seq1, seq2, aligned = False,
+                ignore_gaps = False)
+        self.assertAlmostEqual(dps, 3 / float(13))
+
+        seq1 = 'ATCCGT'
+        seq2 = 'ACCGT'
+        d = seqstats.distance(seq1, seq2, aligned = False,
+                ignore_gaps = True)
+        self.assertEqual(d, 0)
+        dps = seqstats.per_site_distance(seq1, seq2, aligned = False,
+                ignore_gaps = True)
+        self.assertEqual(dps, 0.0)
+        d = seqstats.distance(seq1, seq2, aligned = False,
+                ignore_gaps = False)
+        self.assertEqual(d, 1)
+        dps = seqstats.per_site_distance(seq1, seq2, aligned = False,
+                ignore_gaps = False)
+        self.assertEqual(dps, 1 / float(6))
+
+class GetDifferencesTestCase(unittest.TestCase):
+    def test_align_error(self):
+        seq1 = 'ACGT'
+        seq2 = 'TACGT'
+        self.assertRaises(seqstats.AlignmentError, seqstats.get_differences, seq1, seq2,
+                True)
+        diffs, l = seqstats.get_differences(seq1, seq2)
+        self.assertEqual(diffs, {})
+        self.assertEqual(l, 5)
+        expected = {0: ('-', 'T')}
+        diffs, l = seqstats.get_differences(seq1, seq2, ignore_gaps = False)
+        self.assertEqual(diffs, expected)
+        self.assertEqual(l, 5)
+
+    def test_aligned(self):
+        seq1 = 'AC--GTNAC-TYATR'
+        seq2 = 'ACN-GTAAC--CATT'
+        e = {14:('R', 'T')}
+        diffs, l = seqstats.get_differences(seq1, seq2, aligned = True)
+        self.assertEqual(diffs, e)
+        self.assertEqual(l, 15)
+        diffs, l = seqstats.get_differences(seq1, seq2, aligned = True,
+                ignore_gaps = False)
+        e = {14: ('R', 'T'),
+             2:  ('-', 'N'),
+             10: ('T', '-')}
+        self.assertEqual(diffs, e)
+        self.assertEqual(l, 15)
+
+    def test_unaligned(self):
+        seq1 = 'AC--GTNAC-TYATR'
+        seq2 = 'ACN-GTAAC--CATT'
+        diffs, l = seqstats.get_differences(seq1, seq2, aligned = False,
+                ignore_gaps = False)
+        e = {12: ('R', 'T'),
+             2:  ('-', 'N'),
+             8: ('T', '-')}
+        self.assertEqual(diffs, e)
+        self.assertEqual(l, 13)
+
+        seq1 = 'ATCCGT'
+        seq2 = 'ACCGT'
+        diffs, l = seqstats.get_differences(seq1, seq2, aligned = False,
+                ignore_gaps = True)
+        self.assertEqual(diffs, {})
+        self.assertEqual(l, 6)
+        diffs, l = seqstats.get_differences(seq1, seq2, aligned = False,
+                ignore_gaps = False)
+        self.assertEqual(diffs, {1: ('T', '-')})
+        self.assertEqual(l, 6)
 
 class ColumnFrequenciesTestCase(SeqSiftTestCase):
     def setUp(self):
@@ -32,13 +123,14 @@ class ColumnFrequenciesTestCase(SeqSiftTestCase):
                 SeqRecord(Seq('ACGT?'), id='5')]
 
     def test_alignment_error(self):
-        self.assertRaises(AlignmentError, column_frequencies, self.unaligned)
+        self.assertRaises(seqstats.AlignmentError, seqstats.column_frequencies,
+                self.unaligned)
     
     def test_unambiguous_bases(self):
         for i, char in enumerate(['A', 'C', 'G', 'T']):
             expected_freqs = [0.0] * 5
             expected_freqs[i] = 1.0
-            col_freqs, seqs = column_frequencies(
+            col_freqs, seqs = seqstats.column_frequencies(
                     self.simple_alignment,
                     character_list=[char])
             self.assertEqual(
@@ -48,14 +140,14 @@ class ColumnFrequenciesTestCase(SeqSiftTestCase):
 
     def test_gap_chars(self):
         expected_freqs = ([0.0] * 4) + [1.0]
-        col_freqs, seqs = column_frequencies(
+        col_freqs, seqs = seqstats.column_frequencies(
                 self.simple_alignment)
         self.assertEqual(
                 col_freqs,
                 expected_freqs)
         self.assertSameSequences(self.simple_alignment, seqs)
         expected_freqs = ([0.0] * 4) + [3/float(5)]
-        col_freqs, seqs = column_frequencies(
+        col_freqs, seqs = seqstats.column_frequencies(
                 self.simple_alignment,
                 character_list = ['?'])
         self.assertAlmostEqual(
@@ -65,7 +157,7 @@ class ColumnFrequenciesTestCase(SeqSiftTestCase):
     
     def test_missing_character(self):
         expected_freqs = [0.0] * 5
-        col_freqs, seqs = column_frequencies(
+        col_freqs, seqs = seqstats.column_frequencies(
                 self.simple_alignment,
                 character_list = ['x'])
         self.assertAlmostEqual(
