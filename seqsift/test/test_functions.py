236c962ef75497c625db24aedc16124e6ab81c0f
Jamie Oaks <joaks1@gmail.com>
2014-01-05 01:20:27 -0800

    Updating `sample_iter` function.
    Adding to additional arguments: `exclude` and `exclude_attribute`.  The
    former is a list of elements to exclude when sampling from the
    population, and the latter is the attribute to use to test whether an
    element is in the exclusion list. The default for `exclude_attribute` is
    `None`, in which case the exclude list is checked for the element
    itself.
    
    Adding additional test to `test_functions.py` that test the new
    functionality of `sample_iter`.

--- seqsift/test/test_functions.py
+++ seqsift/test/test_functions.py
@@ -24,10 +24,38 @@ class TestSampleIter(unittest.TestCase):
         total = sum(counts)
         self.assertEqual(total, 5 * 100000)
         freqs = [i/float(total) for i in counts]
-        print freqs
         for f in freqs:
             self.assertAlmostEqual(f, 1/float(10), 2)
 
+    def test_sample_iter_exclude(self):
+        counts = [0 for i in range(10)]
+        for i in range(100000):
+            iterable = (x for x in range(10))
+            s = functions.sample_iter(iterable, 5, exclude = [9])
+            for j in s:
+                counts[j] += 1
+        total = sum(counts)
+        self.assertEqual(total, 5 * 100000)
+        self.assertEqual(counts[9], 0)
+        freqs = [i/float(total) for i in counts if i > 0]
+        self.assertEqual(len(freqs), 9)
+        for f in freqs:
+            self.assertAlmostEqual(f, 1/float(9), 2)
+
+        counts = [0 for i in range(10)]
+        for i in range(100000):
+            iterable = (x for x in range(10))
+            s = functions.sample_iter(iterable, 5, exclude = [8, 9])
+            for j in s:
+                counts[j] += 1
+        total = sum(counts)
+        self.assertEqual(total, 5 * 100000)
+        self.assertEqual(counts[9], 0)
+        self.assertEqual(counts[8], 0)
+        freqs = [i/float(total) for i in counts if i > 0]
+        self.assertEqual(len(freqs), 8)
+        for f in freqs:
+            self.assertAlmostEqual(f, 1/float(8), 2)
 
 if __name__ == '__main__':
     unittest.main()
--- seqsift/utils/functions.py
+++ seqsift/utils/functions.py
@@ -6,19 +6,32 @@ import random
 
 from seqsift.utils import GLOBAL_RNG
 
-def sample_iter(iterable, sample_size, rng = None):
+def sample_iter(iterable, sample_size, exclude = [], exclude_attribute = None,
+        rng = None):
+
+    def get_attribute(el, attribute=None):
+        if attribute is None:
+            return el
+        return getattr(el, attribute)
+
+    exclude = [get_attribute(x, exclude_attribute) for x in exclude]
     if not rng:
         rng = GLOBAL_RNG
     samples = []
     iterator = iter(iterable)
     try:
         for i in xrange(sample_size):
-            samples.append(iterator.next())
+            s = iterator.next()
+            while get_attribute(s, exclude_attribute) in exclude:
+                s = iterator.next()
+            samples.append(s)
     except StopIteration:
         raise ValueError("Sample size {0} is larger than population".format(
                 sample_size))
     rng.shuffle(samples)
     for idx, el in enumerate(iterator, sample_size):
+        if get_attribute(el, exclude_attribute) in exclude:
+            continue
         r = rng.randint(0, idx)
         if r < sample_size:
             samples[r] = el
