8b5ea3eb05749300d12afe98f930419ceccd9e77
Jamie Oaks <joaks1@gmail.com>
2014-01-05 19:25:20 -0800

    Adding `argparse_utils.py` module.

---
+++ seqsift/utils/argparse_utils.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+
+import os
+import argparse
+
+from seqsift.utils.fileio import expand_path
+
+def arg_is_path(path):
+    try:
+        if not os.path.exists(path):
+            raise
+    except:
+        msg = 'path {0!r} does not exist'.format(path)
+        raise argparse.ArgumentTypeError(msg)
+    return expand_path(path)
+
+def arg_is_file(path):
+    try:
+        if not os.path.isfile(path):
+            raise
+    except:
+        msg = '{0!r} is not a file'.format(path)
+        raise argparse.ArgumentTypeError(msg)
+    return expand_path(path)
+
+def arg_is_dir(path):
+    try:
+        if not os.path.isdir(path):
+            raise
+    except:
+        msg = '{0!r} is not a directory'.format(path)
+        raise argparse.ArgumentTypeError(msg)
+    return expand_path(path)
+
