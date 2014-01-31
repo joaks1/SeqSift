20b52004a2527938c1b1d9a82a6c2105a5b19d01
Jamie Oaks <joaks1@gmail.com>
2014-01-31 00:51:07 -0800

    Adding tests of `translate_seqs` function.
    Adding tests of `translate_seqs` function to `test_seqmod.py`.  Also
    importing `seqmod` module within `test_seqmod.py` rather than importing
    everything via wildcard (*).
    
    All tests are passing.

--- seqsift/test/test_seqmod.py
+++ seqsift/test/test_seqmod.py
@@ -10,7 +10,7 @@ from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 from Bio.Alphabet import IUPAC
 
-from seqsift.seqops.seqmod import *
+from seqsift.seqops import seqmod
 from seqsift.test.support import package_paths
 from seqsift.test.support.extended_test_case import SeqSiftTestCase
 from seqsift.utils.messaging import get_logger
@@ -26,21 +26,21 @@ class SeqModTestCase(SeqSiftTestCase):
                 SeqRecord(Seq('ACGT'), id='4'),]
     
     def test_default(self):
-        new_seqs = seq_mod(self.seqs)
+        new_seqs = seqmod.seq_mod(self.seqs)
         self.assertIsInstance(new_seqs, types.GeneratorType)
         self.assertSameData(new_seqs, self.seqs)
 
     def test_unbalanced_strings(self):
-        new_seqs = seq_mod(self.seqs, 'A', 'GT')
+        new_seqs = seqmod.seq_mod(self.seqs, 'A', 'GT')
         self.assertRaises(ValueError, list, new_seqs)
 
     def test_simple_change(self):
-        new_seqs = seq_mod(self.seqs, 'T', 'A')
+        new_seqs = seqmod.seq_mod(self.seqs, 'T', 'A')
         expected = [SeqRecord(str(s.seq).replace('T','A'), id=s.id) for s in self.seqs]
         self.assertSameData(new_seqs, expected)
 
     def test_simple_deletion(self):
-        new_seqs = seq_mod(self.seqs, del_chars='T')
+        new_seqs = seqmod.seq_mod(self.seqs, del_chars='T')
         expected = [SeqRecord(str(s.seq).replace('T',''), id=s.id) for s in self.seqs]
         self.assertSameData(new_seqs, expected)
 
@@ -53,9 +53,55 @@ class RemoveGapsTestCase(SeqSiftTestCase):
                 SeqRecord(Seq('A-CGT'), id='4'),]
 
     def test_simple(self):
-        new_seqs = remove_gaps(self.seqs)
+        new_seqs = seqmod.remove_gaps(self.seqs)
         expected = [SeqRecord(str(s.seq).replace('-',''), id=s.id) for s in self.seqs]
         self.assertSameData(new_seqs, expected)
+
+class TranslateSeqsTestCase(SeqSiftTestCase):
+    def test_dna(self):
+        seqs = [
+                SeqRecord(Seq('ATGACCAACTGAATA', IUPAC.ambiguous_dna), id='1'),
+                SeqRecord(Seq('ATGACCAACTGACAC', IUPAC.ambiguous_dna), id='2'),
+                SeqRecord(Seq('ATGACCAACTGACCC', IUPAC.ambiguous_dna), id='3'),
+                ]
+        exp = [
+                SeqRecord(Seq('MTN*I', IUPAC.protein), id='1'),
+                SeqRecord(Seq('MTN*H', IUPAC.protein), id='2'),
+                SeqRecord(Seq('MTN*P', IUPAC.protein), id='3'),
+                ]
+        new_seqs = seqmod.translate_seqs(seqs)
+        self.assertIsInstance(new_seqs, types.GeneratorType)
+        self.assertSameData(new_seqs, exp)
+
+    def test_dna_gaps(self):
+        seqs = [
+                SeqRecord(Seq('A-TGAC--CAACTG--AATA-', IUPAC.ambiguous_dna), id='1'),
+                SeqRecord(Seq('---ATGACCAACTGACAC---', IUPAC.ambiguous_dna), id='2'),
+                SeqRecord(Seq('ATGACC-AA-C--TGACCC', IUPAC.ambiguous_dna), id='3'),
+                ]
+        exp = [
+                SeqRecord(Seq('MTN*I', IUPAC.protein), id='1'),
+                SeqRecord(Seq('MTN*H', IUPAC.protein), id='2'),
+                SeqRecord(Seq('MTN*P', IUPAC.protein), id='3'),
+                ]
+        new_seqs = seqmod.translate_seqs(seqs)
+        self.assertIsInstance(new_seqs, types.GeneratorType)
+        self.assertSameData(new_seqs, exp)
+
+    def test_dna_extra_bases(self):
+        seqs = [
+                SeqRecord(Seq('ATGACCAACTGAATAA', IUPAC.ambiguous_dna), id='1'),
+                SeqRecord(Seq('ATGACCAACTGACACTA', IUPAC.ambiguous_dna), id='2'),
+                SeqRecord(Seq('ATGACCAACTGACCCAC', IUPAC.ambiguous_dna), id='3'),
+                ]
+        exp = [
+                SeqRecord(Seq('MTN*I', IUPAC.protein), id='1'),
+                SeqRecord(Seq('MTN*H', IUPAC.protein), id='2'),
+                SeqRecord(Seq('MTN*P', IUPAC.protein), id='3'),
+                ]
+        new_seqs = seqmod.translate_seqs(seqs)
+        self.assertIsInstance(new_seqs, types.GeneratorType)
+        self.assertSameData(new_seqs, exp)
 
 if __name__ == '__main__':
     unittest.main()
