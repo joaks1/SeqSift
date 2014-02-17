e290a745cdef94d80bbee731fdb47f58d361303c
Jamie Oaks <joaks1@gmail.com>
2014-02-16 19:02:51 -0800

    Adding `duplicate_id_filter` function.
    Adding `duplicate_id_filter` function to `seqfilter` module, and adding
    associated tests to `test_seqfilter.py`.

--- seqsift/seqops/seqfilter.py
+++ seqsift/seqops/seqfilter.py
@@ -4,11 +4,28 @@ import sys
 import os
 import itertools
 
-from seqsift.seqops.seqstats import column_frequencies
-from seqsift.seqops.sequtils import copy_seq_metadata
+from seqsift.seqops import sequtils, seqstats
+from seqsift.utils import dataio
 from seqsift.utils.messaging import get_logger
 
 _LOG = get_logger(__name__)
+
+def duplicate_id_filter(seq_iter):
+    ids = set()
+    checked_seqs = dataio.BufferedIter()
+    for seq in seq_iter:
+        if seq.id in ids:
+            for prev_seq in checked_seqs:
+                if prev_seq.id == seq.id:
+                    break
+            if not sequtils.sequences_are_equal(prev_seq, seq):
+                    raise Exception('Found {0} more than once, but with '
+                            'different data!'.format(seq.id))
+            continue
+        else:
+            checked_seqs.append(seq)
+        ids.add(seq.id)
+        yield seq
 
 def length_filter(seq_iter, min_length=0, max_length=None):
     if max_length and (max_length < min_length):
@@ -22,12 +39,12 @@ def length_filter(seq_iter, min_length=0, max_length=None):
                 yield seq
 
 def column_filter(seq_iter, character_list=['?','-'], max_frequency=1.0):
-    col_freqs, seqs = column_frequencies(seq_iter,
+    col_freqs, seqs = seqstats.column_frequencies(seq_iter,
             character_list=character_list)
     cols_to_keep = [p < max_frequency for p in col_freqs]
     for seq in seqs:
         new_seq = itertools.compress(str(seq.seq), cols_to_keep)
-        yield copy_seq_metadata(seq, new_seq=''.join(new_seq))
+        yield sequtils.copy_seq_metadata(seq, new_seq=''.join(new_seq))
 
 def row_filter(seq_iter, character_list=['?','-'], max_frequency=1.0):
     char_list = [c.lower() for c in character_list]
--- seqsift/test/test_seqfilter.py
+++ seqsift/test/test_seqfilter.py
@@ -10,13 +10,79 @@ from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 from Bio.Alphabet import IUPAC
 
-from seqsift.seqops.seqfilter import *
+from seqsift.seqops import seqfilter
 from seqsift.utils.errors import AlignmentError
 from seqsift.test.support import package_paths
 from seqsift.test.support.extended_test_case import SeqSiftTestCase
 from seqsift.utils.messaging import get_logger
 
 _LOG = get_logger(__name__)
+
+class DuplicateIdFilterTestCase(SeqSiftTestCase):
+    def test_no_dups(self):
+        seqs = [
+                SeqRecord(Seq('ACGT'), id='a'),
+                SeqRecord(Seq('ACGTA'), id='b'),
+                SeqRecord(Seq('ACGTAC'), id='c'),
+                SeqRecord(Seq('ACGTACG'), id='d'),
+                SeqRecord(Seq('ACGTACGT'), id='e')]
+        ret = list(seqfilter.duplicate_id_filter(seqs))
+        self.assertSameData(seqs, ret)
+
+    def test_one_dup(self):
+        seqs = [
+                SeqRecord(Seq('ACGT'), id='a'),
+                SeqRecord(Seq('ACGTA'), id='b'),
+                SeqRecord(Seq('ACGTAC'), id='c'),
+                SeqRecord(Seq('ACGTACG'), id='d'),
+                SeqRecord(Seq('ACGT'), id='a'),
+                SeqRecord(Seq('ACGTACGT'), id='e')]
+        exp = [
+                SeqRecord(Seq('ACGT'), id='a'),
+                SeqRecord(Seq('ACGTA'), id='b'),
+                SeqRecord(Seq('ACGTAC'), id='c'),
+                SeqRecord(Seq('ACGTACG'), id='d'),
+                SeqRecord(Seq('ACGTACGT'), id='e')]
+        ret = list(seqfilter.duplicate_id_filter(seqs))
+        self.assertSameData(ret, exp)
+
+    def test_mult_dup(self):
+        seqs = [
+                SeqRecord(Seq('ACGT'), id='a'),
+                SeqRecord(Seq('ACGTA'), id='b'),
+                SeqRecord(Seq('ACGTAC'), id='c'),
+                SeqRecord(Seq('ACGTA'), id='b'),
+                SeqRecord(Seq('ACGTA'), id='b'),
+                SeqRecord(Seq('ACGTA'), id='b'),
+                SeqRecord(Seq('ACGTACG'), id='d'),
+                SeqRecord(Seq('ACGT'), id='a'),
+                SeqRecord(Seq('ACGT'), id='a'),
+                SeqRecord(Seq('ACGT'), id='a'),
+                SeqRecord(Seq('ACGTAC'), id='c'),
+                SeqRecord(Seq('ACGTAC'), id='c'),
+                SeqRecord(Seq('ACGT'), id='a'),
+                SeqRecord(Seq('ACGTAC'), id='c'),
+                SeqRecord(Seq('ACGTAC'), id='c'),
+                SeqRecord(Seq('ACGTACGT'), id='e')]
+        exp = [
+                SeqRecord(Seq('ACGT'), id='a'),
+                SeqRecord(Seq('ACGTA'), id='b'),
+                SeqRecord(Seq('ACGTAC'), id='c'),
+                SeqRecord(Seq('ACGTACG'), id='d'),
+                SeqRecord(Seq('ACGTACGT'), id='e')]
+        ret = list(seqfilter.duplicate_id_filter(seqs))
+        self.assertSameData(ret, exp)
+
+    def test_dup_error(self):
+        seqs = [
+                SeqRecord(Seq('ACGT'), id='a'),
+                SeqRecord(Seq('ACGTA'), id='b'),
+                SeqRecord(Seq('ACGTAC'), id='c'),
+                SeqRecord(Seq('ACGTACG'), id='d'),
+                SeqRecord(Seq('ACGTA'), id='a'),
+                SeqRecord(Seq('ACGTACGT'), id='e')]
+        ret = seqfilter.duplicate_id_filter(seqs)
+        self.assertRaises(Exception, list, ret)
 
 class LengthFilterTestCase(unittest.TestCase):
     def setUp(self):
@@ -28,44 +94,44 @@ class LengthFilterTestCase(unittest.TestCase):
                 SeqRecord(Seq('ACGTACGT'), id='8')]
     
     def test_limit_error(self):
-        f = length_filter([], 6, 5)
+        f = seqfilter.length_filter([], 6, 5)
         self.assertRaises(ValueError, f.next)
 
     def test_default(self):
-        filter_iter = length_filter(self.seqs)
+        filter_iter = seqfilter.length_filter(self.seqs)
         self.assertIsInstance(filter_iter, types.GeneratorType)
         filtrate = list(filter_iter)
         self.assertEqual(len(filtrate), len(self.seqs))
         self.assertEqual(self.seqs, filtrate)
 
     def test_no_filter_limit(self):
-        filtrate = list(length_filter(self.seqs, 4, 8))
+        filtrate = list(seqfilter.length_filter(self.seqs, 4, 8))
         self.assertEqual(len(filtrate), len(self.seqs))
         self.assertEqual(self.seqs, filtrate)
 
     def test_no_max(self):
         for i in range(4, 9):
-            filtrate = list(length_filter(self.seqs, i))
+            filtrate = list(seqfilter.length_filter(self.seqs, i))
             self.assertEqual(len(filtrate), 9-i)
 
     def test_no_min(self):
         for i in range(4, 9):
-            filtrate = list(length_filter(self.seqs, max_length=i))
+            filtrate = list(seqfilter.length_filter(self.seqs, max_length=i))
             self.assertEqual(len(filtrate), i-3)
 
     def test_equal_min_max(self):
-        filtrate = list(length_filter(self.seqs, 6, 6))
+        filtrate = list(seqfilter.length_filter(self.seqs, 6, 6))
         self.assertEqual(len(filtrate), 1)
         self.assertEqual(filtrate[0], self.seqs[2])
     
     def test_empty_iter(self):
-        filtrate = list(length_filter([]))
+        filtrate = list(seqfilter.length_filter([]))
         self.assertEqual([], filtrate)
 
     def test_no_filtrate(self):
-        filtrate = list(length_filter(self.seqs, 9, 10))
+        filtrate = list(seqfilter.length_filter(self.seqs, 9, 10))
         self.assertEqual([], filtrate)
-        filtrate = list(length_filter(self.seqs, 0, 3))
+        filtrate = list(seqfilter.length_filter(self.seqs, 0, 3))
         self.assertEqual([], filtrate)
 
 class ColumnFilterTestCase(SeqSiftTestCase):
@@ -84,7 +150,7 @@ class ColumnFilterTestCase(SeqSiftTestCase):
                 SeqRecord(Seq('ACGT?'), id='5')]
 
     def test_alignment_error(self):
-        f = column_filter(self.unaligned)
+        f = seqfilter.column_filter(self.unaligned)
         self.assertRaises(AlignmentError, f.next)
 
     def test_remove_gap(self):
@@ -95,27 +161,27 @@ class ColumnFilterTestCase(SeqSiftTestCase):
                 SeqRecord(Seq('ACGT'), id='4'),
                 SeqRecord(Seq('ACGT'), id='5')]
 
-        seqs = list(column_filter(self.simple_alignment,
+        seqs = list(seqfilter.column_filter(self.simple_alignment,
                 character_list = ['?', '-'],
                 max_frequency = 1.0))
         self.assertSameData(seqs, expected)
 
-        seqs = list(column_filter(self.simple_alignment,
+        seqs = list(seqfilter.column_filter(self.simple_alignment,
                 character_list = ['-'],
                 max_frequency = 2/float(5)))
         self.assertSameData(seqs, expected)
 
-        seqs = list(column_filter(self.simple_alignment,
+        seqs = list(seqfilter.column_filter(self.simple_alignment,
                 character_list = ['?'],
                 max_frequency = 3/float(5)))
         self.assertSameData(seqs, expected)
 
-        seqs = list(column_filter(self.simple_alignment,
+        seqs = list(seqfilter.column_filter(self.simple_alignment,
                 character_list = ['-'],
                 max_frequency = 3/float(5)))
         self.assertSameData(seqs, self.simple_alignment)
 
-        seqs = list(column_filter(self.simple_alignment,
+        seqs = list(seqfilter.column_filter(self.simple_alignment,
                 character_list = ['?'],
                 max_frequency = 3.1/float(5)))
         self.assertSameData(seqs, self.simple_alignment)
@@ -136,28 +202,28 @@ class RowFilterTestCase(SeqSiftTestCase):
                 SeqRecord(Seq('ACGT?'), id='5')]
         
     def test_clean(self):
-        seqs = list(row_filter(self.unaligned,
+        seqs = list(seqfilter.row_filter(self.unaligned,
                 character_list=['?', '-'],
                 max_frequency=0.5))
         self.assertSameData(seqs, self.unaligned)
-        seqs = list(row_filter(self.simple_alignment,
+        seqs = list(seqfilter.row_filter(self.simple_alignment,
                 character_list=['?', '-'],
                 max_frequency=0.5))
         self.assertSameData(seqs, self.simple_alignment)
 
     def test_simple(self):
-        seqs = list(row_filter(self.unaligned,
+        seqs = list(seqfilter.row_filter(self.unaligned,
             character_list=['a'],
             max_frequency = 2/float(5)))
         self.assertSameData(seqs, self.unaligned[:1] + self.unaligned[2:])
 
-        seqs = list(row_filter(self.simple_alignment,
+        seqs = list(seqfilter.row_filter(self.simple_alignment,
             character_list=['-'],
             max_frequency = 1/float(5)))
         self.assertSameData(seqs, self.simple_alignment[::2])
         
     def test_no_filtrate(self):
-        seqs = list(row_filter(self.simple_alignment,
+        seqs = list(seqfilter.row_filter(self.simple_alignment,
             character_list=['?','-'],
             max_frequency = 1/float(5)))
         self.assertEqual(seqs, [])
